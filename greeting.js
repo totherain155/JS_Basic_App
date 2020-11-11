1113e128149b06a7e970f92a7f3b0a734bb16f6a
totherain155 <totherain155@gmail.com>
2020-11-11 18:07:29 +0900

    review

--- greeting.js
+++ greeting.js
@@ -1,52 +1,55 @@
 const form = document.querySelector(".js-form"),
    input = form.querySelector("input"),
-   greeting = document.querySelector(".js-greetings");
+   greetings = document.querySelector(".js-greetings");
 
-   const USER_LS = "tempUser",
-       SHOWING_CN = "showing";
+const LS_NAME = "user";
+const SHOW_CLASS = "showing"
 
-   function saveName(name){
-      localStorage.setItem(USER_LS, name);
-   }
+function saveName(name){
+   localStorage.setItem(LS_NAME, `${name}`)
 
-   
-   function handleSubmit(event){
-      event.preventDefault();
-      const currentValue = input.value;
-      ShowYourName(currentValue);
-      form.classList.remove(SHOWING_CN); //showYourName함수 속이 아닌 이곳에 포함시켜 보았다. 
-      saveName(currentValue);
-   }
+}
 
-   function askForName(){
-      form.classList.add(SHOWING_CN);
-      form.addEventListener("submit", handleSubmit);
-
-   }    
+function handleSubmit(event){
+   event.preventDefault();
+   const currentValue = input.value;
+   showYourName(currentValue);
+   form.classList.remove(SHOW_CLASS);
+   saveName(currentValue);
 
 
-   function ShowYourName(name){
-      //form.classList.remove(SHOWING_CN);
-      greeting.classList.add(SHOWING_CN);
-      greeting.innerText = `Good to see you ${name}!!`
+}
 
-   }
-
-
-   function loadName(){
-      const currentUser = localStorage.getItem(USER_LS);
-      if(currentUser === null){
-          askForName();  // localStorage가 비어있다면 이름을 물어보자.
-      }else{
-          ShowYourName(currentUser); // localStorage에 이름이 있다면 화면에 표시한다.
-      }
-   }
+function askForName(){
+    form.classList.add(SHOW_CLASS);  
+    form.addEventListener("submit", handleSubmit);   
+      
+}
 
 
+function showYourName(name){
+   greetings.classList.add(SHOW_CLASS);
+   greetings.innerText = `welcome ${name}`
 
-   function init(){
-      loadName();
+}
 
-   }
 
-   init();
+function loadName(){
+    const currentName = localStorage.getItem(LS_NAME);
+    if(currentName === null){
+        askForName();
+    }else{
+        showYourName(currentName);
+    }
+
+}   
+
+
+
+function init(){
+    loadName();
+
+}
+
+
+init();
